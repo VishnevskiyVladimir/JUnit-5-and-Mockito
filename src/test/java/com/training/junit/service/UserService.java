f589a5847cc7061f97a86217b441cc0710a9afa4
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-11 19:10:24 +0300

    First commit (First small problem) and add .gitignore
    Maven->Lificycle->test   doesn't show that test droped.

---
+++ src/test/java/com/training/junit/service/UserService.java
@@ -0,0 +1,11 @@
+package com.training.junit.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserService {
+    @Test
+    void test(){
+        Assertions.assertTrue(false);
+    }
+}
