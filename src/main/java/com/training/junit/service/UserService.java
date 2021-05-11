d1c830c2ff3e8282c8e2973c67a7d50874e027a9
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-11 21:27:45 +0300

    test lifecycle
    BeforeEach, AfterEach
    BeforeAll,AfterAll should be static
    @TestInstance(TestInstance.Lifecycle.PER_METHOD) - default. If we set PER_CLASS new userService will not be created before each test

--- src/main/java/com/training/junit/service/UserService.java
+++ src/main/java/com/training/junit/service/UserService.java
@@ -2,13 +2,19 @@ package com.training.junit.service;
 
 import com.training.junit.dto.User;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserService {
 
+    private final List<User> users = new ArrayList<>();
+
     public List<User> getAll() {
-        return Collections.emptyList();
+        return new ArrayList<>(users);
+    }
+
+    public void add(User user) {
+        users.add(user);
     }
 }
 
--- src/test/java/com/training/junit/service/UserServiceTest.java
+++ src/test/java/com/training/junit/service/UserServiceTest.java
@@ -1,17 +1,48 @@
 package com.training.junit.service;
 
 import com.training.junit.dto.User;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeAll
+    static void deleteDataFromDb() {
+        System.out.println("Before All");
+    }
+
+    @BeforeEach
+    void closeConnectionPool() {
+        System.out.println("Before Each" + this);
+        userService = new UserService();
+    }
 
     @Test
     void usersEmptyIfNoUsersAdded(){
-        var userService = new UserService();
+        System.out.println("test1" + this);
         var users = userService.getAll();
         Assertions.assertTrue(users.isEmpty());
+    }
+
+    @Test
+    void usersSizeIfUserAdded(){
+        System.out.println("test2" + this);
+        userService.add(new User());
+        userService.add(new User());
+        var users = userService.getAll();
+        Assertions.assertEquals(2,users.size());
+    }
+
+    @AfterEach
+    void clean() {
+        System.out.println("After Each" + this);
+    }
+
+    @AfterAll
+    static void cleanFinal() {
+        System.out.println("After All");
     }
 }
