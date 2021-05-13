74b05c9d503b18aad0de4cd764428e8b1a407f01
Vladimir Vishnevskiy <vishnevskiy.vladimir.mbox@gmail.com>
2021-05-13 18:22:28 +0300

    delete private (not needed with lombok @value)

--- src/main/java/com/training/junit/dto/User.java
+++ src/main/java/com/training/junit/dto/User.java
@@ -5,8 +5,8 @@ import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class User {
-    private long id;
-    private String username;
-    private String password;
+    long id;
+    String username;
+    String password;
 
 }
